ab422cf9e65bc8c635fd514566337a074dba9321
Burak Sipahi <ameisecreative@gmail.com>
2021-04-06 01:54:16 +0300

    Add files via upload

---
+++ KupOdevi/KupBilgileri.java
@@ -0,0 +1,16 @@
+package KupOdevi;
+
+public class KupBilgileri {
+	int ayritUzunlugu;
+	int kenarUzunlugu;
+	KupBilgileri() {
+		
+	}
+	KupBilgileri(int ayritUzunlugu,int kenarUzunlugu) {
+	
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ KupOdevi/Main.java
@@ -0,0 +1,23 @@
+package KupOdevi;
+
+import java.util.Scanner;
+
+import javax.swing.text.StyledEditorKit.AlignmentAction;
+
+
+public class Main {
+public static void main(String[] args) {
+	
+	KupBilgileri kupbilgileri = new KupBilgileri(); //Kup Bilgileri Constructor'ýna eriþmek için nesneyi oluþturuyoruz
+	Scanner scanner = new Scanner(System.in); // Kullanýcýdan veri almak için scanner olustuyoruz
+	System.out.println("Küpün Bir Kenar Uzunluðunu Giriniz : ");
+	kupbilgileri.kenarUzunlugu = scanner.nextInt(); // kullanýcýdan veriyi alýyoruz
+	Hesaplama hesaplama = new Hesaplama(); //Hesaplama Constructor'ýna eriþmek için nesneyi oluþturuyoruz
+	hesaplama.kenarUzunluk = kupbilgileri.kenarUzunlugu; //hesaplamadaki kenarUzunluk deðiþkenine iþlem yapabilmesi için KüpBilgilerindeki kenarUzunluðu verisini atýyoruz
+	System.out.println("Hacim = "+hesaplama.hacimHesapla()+" Birim³"); //Hesaplama classýndaki iþlem yap metodunu çaðýrýp iþlemin sonucunu gosterýyoruz
+	System.out.println("Alan = "+hesaplama.alanHesapla()+" Birim²");
+	System.out.println("Çevre = "+hesaplama.cevreHesapla()+" Birim");
+	
+		
+	}
+}
